686e312b824df791152e060f23ef6842adebdb80
Denis <antongetse@gmail.com>
2023-03-02 16:36:28 +0300

    New programm OOP_JAVA_1

---
+++ Main.java
@@ -0,0 +1,45 @@
+package JavaOOP1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person irina = new Person("Ирина", 56);
+        Person igor = new Person("Игорь", 60);
+        Person vasya = new Person("Вася", 30);
+        Person masha = new Person("Маша", 27);
+        Person jane = new Person("Женя", 10);
+        Person ivan = new Person("Ваня", 8);
+        Person Vlada = new Person("Влада", 9);
+        Person Maksim = new Person("Максим", 10);
+        Person Oleg = new Person("Олег", 11);
+        Person Sasha = new Person("Саша", 12);
+        Person vlad = new Person("Влад", 3);
+        GeoTree gt = new GeoTree();
+        gt.appendPerentChild(irina, vasya); // Ирина родитель Васи
+        gt.appendPerentChild(irina, masha); // Ирина родитель Маши
+        gt.appendPerentChild(vasya, jane); // Вася родитель Жени
+        gt.appendPerentChild(vasya, ivan); // Вася родитель Ивана
+        gt.appendVifeHusbent(irina, igor); // Ирина жена Игоря
+        gt.appendPerentChild(igor, vasya); // Игорь родитель Васи
+        gt.appendPerentChild(igor, masha); // Игорь родитель Маши
+        gt.appendPerentChild(igor, vlad); // Игорь родитель Влада
+        gt.appendPerentChild(igor, Maksim); // Игорь родитель Максима
+        gt.appendPerentChild(igor, Oleg); // Игорь родитель Олега
+        gt.appendPerentChild(igor, Vlada); // Игорь родитель Влада 
+        gt.appendPerentChild(igor, Sasha); // Игорь родитель Саши      
+
+        // Ищем детей Ирины
+        System.out.println("Дети Ирины");
+        System.out.println(new Research(gt).spend(irina, Relationship.parent));
+        // Ищем детей Игоря
+        System.out.println("Дети Игоря");
+        System.out.println(new Research(gt).spend(igor, Relationship.parent));
+
+        // Ищем мужа Ирины (Чья жена Ирина?)
+        System.out.println("Муж Ирины");
+        System.out.println(new Research(gt).spend(irina, Relationship.vife));
+
+        // Ищем людей определенного возраста
+        System.out.println(new Research(gt).searchAge());
+    }
+}
---
+++ Relationship.java
@@ -0,0 +1,8 @@
+package JavaOOP1;
+
+public enum Relationship {
+    parent,
+    children,
+    vife,
+    husbent    
+}
